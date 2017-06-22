773dc201398a0aafc2190df05d5c39b2e9a98a22
l85m <landon.mossburg@gmail.com>
2017-06-21 22:51:29 -0700

    cleaning up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyssm',
+    version='0.1',
+    packages=['boto3'],
+    url='https://github.com/Tesla-SCA/pyssm',
+    license='',
+    author='Landon Mossburg',
+    author_email='lmossburg@tesla.com',
+    description='Provides a simple wrapper for getting, working with, and refreshing ssm_params'
+)
