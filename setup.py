e0a2032bae4c4269f33c1fc0131712d5dd2fdb90
l85m <landon.mossburg@gmail.com>
2017-06-21 23:08:42 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyssm',
     version='0.1',
-    packages=['boto3'],
+    packages=find_packages(),
     url='https://github.com/Tesla-SCA/pyssm',
     license='',
     author='Landon Mossburg',
     author_email='lmossburg@tesla.com',
-    description='Provides a simple wrapper for getting, working with, and refreshing ssm_params'
+    description='Provides a simple wrapper for getting, working with, and refreshing ssm_params',
+    install_requires=["boto3"]
 )
